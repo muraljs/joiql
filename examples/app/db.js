f8014faaa583499307a2254c5624e625797fc86a
Artsy <craigspaeth@gmail.com>
2016-11-29 23:32:40 -0500

    reduce noise

--- examples/app/db.js
+++ examples/app/db.js
@@ -4,14 +4,14 @@ const _db = {}
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const db = {
-  findOne: (collection, id) =>
-    delay(50).then(() => _db[collection] && _db[collection][id]),
-  save: (collection, doc) =>
+  findOne: (col, id) =>
+    delay(50).then(() => _db[col] && _db[col][id]),
+  save: (col, doc) =>
     delay(50).then(() => {
       const id = doc.id || uniqueId()
       const res = assign(doc, { id })
-      if (!_db[collection]) _db[collection] = {}
-      _db[collection][id] = res
+      if (!_db[col]) _db[col] = {}
+      _db[col][id] = res
       return res
     })
 }
@@ -20,13 +20,13 @@ exports.persist = (ctx, next) => {
   const finds = map(ctx.req.query, (branch, key) => {
     const isAlias = !branch.args
     const args = isAlias ? values(branch)[0].args : branch.args
-    const collection = isAlias ? keys(branch)[0] : key
+    const col = isAlias ? keys(branch)[0] : key
     return db
-      .findOne(collection, args.id)
+      .findOne(col, args.id)
       .then((doc) => { ctx.res[key] = doc })
   })
-  const saves = map(ctx.req.mutation, ({ args }, collection) =>
-    db.save(collection, args).then((doc) => { ctx.res[collection] = doc }))
+  const saves = map(ctx.req.mutation, ({ args }, col) =>
+    db.save(col, args).then((doc) => { ctx.res[col] = doc }))
   return Promise.all(finds.concat(saves)).then(next)
 }
 
