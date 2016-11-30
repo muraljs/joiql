88a27caa8cb2c88ae2aff7bd2fc88541112f64ac
Artsy <craigspaeth@gmail.com>
2016-11-29 23:32:04 -0500

    update example app to have middleware work with aliases

--- examples/app/db.js
+++ examples/app/db.js
@@ -1,26 +1,32 @@
-const { map, uniqueId, assign } = require('lodash')
+const { map, uniqueId, assign, values, keys } = require('lodash')
 
 const _db = {}
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const db = {
-  findOne: (col, id) =>
-    delay(50).then(() => _db[col] && _db[col][id]),
-  save: (col, doc) =>
+  findOne: (collection, id) =>
+    delay(50).then(() => _db[collection] && _db[collection][id]),
+  save: (collection, doc) =>
     delay(50).then(() => {
       const id = doc.id || uniqueId()
       const res = assign(doc, { id })
-      if (!_db[col]) _db[col] = {}
-      _db[col][id] = res
+      if (!_db[collection]) _db[collection] = {}
+      _db[collection][id] = res
       return res
     })
 }
 
 exports.persist = (ctx, next) => {
-  const finds = map(ctx.req.query, ({ args }, col) =>
-    db.findOne(col, args.id).then((doc) => { ctx.res[col] = doc }))
-  const saves = map(ctx.req.mutation, ({ args }, col) =>
-    db.save(col, args).then((doc) => { ctx.res[col] = doc }))
+  const finds = map(ctx.req.query, (branch, key) => {
+    const isAlias = !branch.args
+    const args = isAlias ? values(branch)[0].args : branch.args
+    const collection = isAlias ? keys(branch)[0] : key
+    return db
+      .findOne(collection, args.id)
+      .then((doc) => { ctx.res[key] = doc })
+  })
+  const saves = map(ctx.req.mutation, ({ args }, collection) =>
+    db.save(collection, args).then((doc) => { ctx.res[collection] = doc }))
   return Promise.all(finds.concat(saves)).then(next)
 }
 
