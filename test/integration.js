9b027bfc6b4b2c600b0cf8f64f18cd286a7b9716
Craig Spaeth <craigspaeth@gmail.com>
2016-12-29 13:35:21 -0500

    support forbidden better

--- test/integration.js
+++ test/integration.js
@@ -1,5 +1,5 @@
 /* eslint-env mocha */
-const { string, number, object, date } = require('joi')
+const { string, number, object, date, array, alternatives } = require('joi')
 const { graphql } = require('graphql')
 const joiql = require('../')
 
@@ -67,6 +67,96 @@ describe('joiql', () => {
     return graphql(schema, '{ hello }').then((res) => {
       (res.data.hello === null).should.be.ok()
       ;(typeof undefined).should.equal('undefined')
+    })
+  })
+
+  it('omits forbidden fields', () => {
+    const schema = joiql({
+      query: {
+        a: string().forbidden(),
+        b: string(),
+        c: object({
+          d: string().forbidden(),
+          e: string()
+        }),
+        f: array().items(object({
+          g: string().forbidden(),
+          h: string()
+        })),
+        j: array().items(
+          object({ k: string() }),
+          object({ l: string() }).forbidden()
+        ),
+        m: alternatives(
+          object({ n: string() }).meta({ name: 'N' }),
+          object({ o: string() }).meta({ name: 'O' }).forbidden()
+        )
+      }
+    })
+    return graphql(schema, '{ a c { d } f { g } j { k l } m { o } }')
+      .then((res) => {
+        const errs = res.errors.map((e) => e.message).join('')
+        errs.should.containEql('Cannot query field "a"')
+        errs.should.containEql('Cannot query field "d"')
+        errs.should.containEql('Cannot query field "g"')
+        errs.should.containEql('Cannot query field "l"')
+        errs.should.containEql('Cannot query field "o"')
+      })
+      .then(() =>
+        graphql(schema, '{ a b c { e } f { h } j { k } m ... on N { n } }')
+      )
+      .then((res) => {
+        const errs = res.errors.map((e) => e.message).join('')
+        errs.should.not.containEql('Cannot query field "b"')
+        errs.should.not.containEql('Cannot query field "e"')
+        errs.should.not.containEql('Cannot query field "h"')
+        errs.should.not.containEql('Cannot query field "k"')
+        errs.should.not.containEql('Cannot query field "n"')
+      })
+  })
+
+  it('omits forbidden args', () => {
+    const schema = joiql({
+      query: {
+        a: string().meta({
+          args: {
+            b: string().forbidden(),
+            c: string(),
+            d: object({
+              e: string().forbidden(),
+              f: string()
+            }),
+            g: array().items(object({
+              h: string().forbidden(),
+              i: string()
+            })),
+            j: array().items(
+              object({ k: string() }),
+              object({ l: string() }).forbidden()
+            ),
+            m: alternatives(
+              object({ n: string() }).meta({ name: 'N' }),
+              object({ o: string() }).meta({ name: 'O' }).forbidden()
+            )
+          }
+        })
+      }
+    })
+    return graphql(schema, `{
+      a(
+        b: "Foo"
+        d: { e: "Foo" }
+        g: { h: "Foo" }
+        j: { l: "Foo" }
+        m: { o: "Foo" }
+      )
+    }`).then((res) => {
+      const errs = res.errors.map((e) => e.message).join('')
+      errs.should.containEql('Unknown argument "b"')
+      errs.should.containEql('has invalid value {e')
+      errs.should.containEql('has invalid value {h')
+      errs.should.containEql('has invalid value {l')
+      errs.should.containEql('has invalid value {o')
     })
   })
 })
